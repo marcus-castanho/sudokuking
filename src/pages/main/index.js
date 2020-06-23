363022240545d26cb249fd933efbf3c2c3851e3f
marcus-castanho <m.castanho@hotmail.com>
2020-06-23 08:12:07 -0300

    commit5 - sdkking

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -18,14 +18,60 @@ export default class Main extends Component {
         for (i = 0; i < 9; i++) {
             matrix[i] = [];
             for (j = 0; j < 9; j++) {
-                matrix[i][j] = Math.trunc(Math.random() * 10);
+                var possibilities = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+                const rowpossibilities = possibilities.filter(cell => (!matrix[i].includes(cell)));
+
+                var t = i;
+                var columnpossibilities = [];
+                while (t >= 0) {
+                    columnpossibilities.push(matrix[t][j]);
+                    t -= 1;
+                }
+
+                columnpossibilities = possibilities.filter(cell => (!columnpossibilities.includes(cell)));
+
+                console.log(columnpossibilities);
+                console.log(rowpossibilities);
+
+                possibilities = rowpossibilities.filter(x => columnpossibilities.includes(x));
+
+                console.log(possibilities);
+
+                matrix[i][j] = possibilities[Math.floor(Math.random() * possibilities.length)];
+
+                console.log(matrix[i][j]);
             }
         }
 
-        await this.setState({ gameTable: matrix }, function () {
-            console.log(this.state.gameTable);/**/
-        });
+        /*if (this.checkGame(matrix) == true) {
+            console.clear();
+            this.generateRamdomGame();
+        }*/
 
+
+        await this.setState({ gameTable: matrix });
+
+    };
+
+    checkGame = (matrix) => {
+        var i = 0;
+        var j = 0;
+        var res = false;
+
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j < 9; j++) {
+                if (matrix[i][j] = undefined) {
+                    res = true;
+                    break;
+                }
+            }
+        }
+        if (res = true) {
+            return true;
+        }
+        else {
+            return false;
+        }
     };
 
     renderTable = () => {
@@ -40,7 +86,7 @@ export default class Main extends Component {
                 {gameTable.map(row => (
                     <tr className="game-row">
                         {row.map(cell => (
-                            <td className="game-cell" id={row.indexOf(cell)}>{cell}</td>
+                            <td className="game-cell" id={gameTable.indexOf(row) + row.indexOf(cell)}>{cell}</td>
                         ))}
                     </tr>
                 ))}
@@ -56,7 +102,10 @@ export default class Main extends Component {
                     <div id='game-header'>
                         <div id='game-info'>
                             <p>Difficulty</p>
-                            <p>01:10:05</p>
+                            <div id='timer'>
+                                <p id='time'>10:00:00</p>
+                                <button id="play-pause-btn">o-</button>
+                            </div>
                         </div>
                         <div id='selec-newgame'>
                             <select>
