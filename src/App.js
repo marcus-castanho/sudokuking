1c7ae156584297e98603147e4a4473bc50a73910
marcus-castanho <m.castanho@hotmail.com>
2020-08-05 00:35:07 -0300

    Sudoku King app v1.2.2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './pages/main';
 import "./style.css";
+import ReactGA from 'react-ga';
+
+const trackingId = "G-9D80N6ZH4D";
+ReactGA.initialize(trackingId);
 
 const App = () => (
   <div className="App">
